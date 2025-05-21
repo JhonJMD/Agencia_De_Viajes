5e52c55fbe20f699da3837f8f6ed7be9bc792c5d
JhonJMD <jjmdcampus@gmail.com>
2025-05-20 19:33:02 -0500

    feat: :rocket: Listo para despliegue

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('agenciaviajes', 'desarrollo', 'yucaconsuero2023', {
-    host: 'localhost',
-    port: 3306,
+const db = new Sequelize('agenciaviajes_hardstory', 'agenciaviajes_hardstory', '17511fe03cbf649825942ce4b89c59f6526604b3', {
+    host: '39nsb.h.filess.io',
+    port: 3307,
     dialect: 'mysql',
     define: {
         timestamps: false
